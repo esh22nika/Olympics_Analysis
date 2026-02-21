b8bf1f6dabe6d595df0d6e6c761f6624843711ae
esh22nika <[eshanikaamballa@gmail.com]>
2026-02-21 23:58:57 +0530

    final

--- preprocessor.py
+++ preprocessor.py
@@ -1,16 +1,12 @@
-import pandas as pd 
+import pandas as pd
 
-df=pd.read_csv('noc_regions.csv')
-region_df=pd.read_csv('athlete_events.csv')
-
-def preprocess(df):
-    df,region_df
-    #filtering for summer olympics
-    df=df[df['Season']=='Summer']
-    #merge with region df
-    df=df.merge(region_df,on='NOC',how='left')
-    #drop duplicates
+def preprocess(df,region_df):
+    # filtering for summer olympics
+    df = df[df['Season'] == 'Summer']
+    # merge with region_df
+    df = df.merge(region_df, on='NOC', how='left')
+    # dropping duplicates
     df.drop_duplicates(inplace=True)
-    #one hot encoding for medals
+    # one hot encoding medals
     df = pd.concat([df, pd.get_dummies(df['Medal'])], axis=1)
     return df
